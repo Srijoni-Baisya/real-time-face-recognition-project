4f72250c1277e47f5b5e49db4f6a928775955262
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-07-21 16:54:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function setup(){
+    canvas = createCanvas(250,250);
+    canvas.center();//place canvas in the center of the screen
+}
+
+function draw(){
+    //update the code in the next project
+}
